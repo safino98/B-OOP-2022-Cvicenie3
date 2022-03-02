d49bd99fb4db9d3ce1973d7667fa4a1e6cb10e64
safinko98 <xsafarp@stuba.sk>
2022-03-02 16:27:45 +0100

    1

---
+++ src/main/java/sk/stuba/fei/uim/oop/Auto.java
@@ -0,0 +1,6 @@
+package sk.stuba.fei.uim.oop;
+
+public class Auto {
+    int rokVyroby;
+    String model;
+}
--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,15 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
+        Auto bmv = new Auto();
+        bmv.model = "x3";
+        bmv.rokVyroby = 1998;
+        System.out.println("BMW, " + bmv.model + " " + bmv.rokVyroby);
+        System.out.println("zadaj nazov auta, model, rok vyroby");
+        String nazov = KeyboardInput.readString("Nazov:");
+        //Auto nazov = new Auto();
+        System.out.println(nazov);
+
 
     }
 }
